85c4d4dd8c509dcd0c88309269538721a3e94ef4
joaorossi15 <joaorossiborba@gmail.com>
2026-09-02 00:29:01 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.WORKSHOP_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw2rXyjW3fMn3KCW9GRJxJqo_i05gbSfFrYVSa8ahJftWClE53W33K6sFS3NRBVDcwx/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzxQJd6vGoyDwJ47mGHzLw8dXxm34NUtpXD6950nxSRgZ2hhJ9pA6XTbt3N1uSeYw8R/exec",
   WORKSHOP_TITLE: "Agente em produção?",
   ORGANIZATION: "Oficina - Guia de Agentes de IA para Serviços Públicos Digitais"
 };
